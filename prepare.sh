1d66f7696c7b7b223a118b4853603d949e908dd6
Nan Dun <dunnan@yl.is.s.u-tokyo.ac.jp>
2009-12-22 12:09:52 +0900

    add prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Prepare libraries for ParaTrac usage
+# Debian/Ubuntu
+
+PKGS="libfuse-dev libglib2.0-dev python python-scipy python-numpy
+python-matplotlib python-networkx"
+
+sudo apt-get install $PKGS
